4bb410c0c43833eb279be2bf89804b5963d82c87
Aameen-SE <aameen.se@gmail.com>
2025-10-28 13:27:05 +0530

    Initial Spring Boot setup with base dependencies

---
+++ src/test/java/com/system/smart_complaint_system/SmartComplaintSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.smart_complaint_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartComplaintSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
